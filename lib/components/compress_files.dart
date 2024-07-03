dad48c5e1bfbed7e0daa362d7a582738033cbdb3
itc-s22020 <s22020@std.it-college.ac.jp>
2024-07-03 11:23:12 +0900

    Webに対応した

--- lib/components/compress_files.dart
+++ lib/components/compress_files.dart
@@ -1,18 +1,17 @@
-import 'dart:io' show File;
 import 'dart:typed_data';
 import 'package:archive/archive.dart';
 
-Future<Uint8List?> compressFiles(List<File> files) async {
+Future<Uint8List?> compressFiles(List<Map<String, dynamic>> files) async {
   final encoder = ZipEncoder();
   final archive = Archive();
 
   for (var file in files) {
-    final Uint8List bytes = await file.readAsBytes();
-    final archiveFile = ArchiveFile(file.path
-        .split('/')
-        .last, bytes.length, bytes);
+    final String fileName = file['name'];
+    final Uint8List bytes = file['data'];
+    final archiveFile = ArchiveFile(fileName, bytes.length, bytes);
     archive.addFile(archiveFile);
   }
+
   final List<int>? zipData = encoder.encode(archive);
 
   if (zipData != null) {
--- lib/components/file_pick.dart
+++ lib/components/file_pick.dart
@@ -1,20 +1,24 @@
-import 'dart:io';
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter/foundation.dart';
 import 'package:get/get.dart';
 import 'package:polygraphy/components/compress_files.dart';
 import 'package:polygraphy/getX/data_controller.dart';
+import 'dart:typed_data';
 
 Future<void> filePick() async {
   final DataController c = Get.put(DataController());
 
   FilePickerResult? result = await FilePicker.platform.pickFiles(
-      allowMultiple: true,
-      withData: true
+    allowMultiple: true,
+    withData: true,
   );
 
   if (result != null && result.files.isNotEmpty) {
-    List<File> files = result.paths.map((path) => File(path!)).toList();
+    List<Map<String, dynamic>> files = result.files.map((file) => {
+      'name': file.name,
+      'data': file.bytes!,
+    }).toList();
+
     Uint8List? zipData = await compressFiles(files);
     if (zipData != null) {
       c.setFile(zipData);
